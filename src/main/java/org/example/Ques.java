7710bdfbb65ede813204f0f0c9ab77af703f36f4
danii <gyfudwvgeyr@>
2025-03-17 01:40:09 +0300

    да-да

--- src/main/java/org/example/Ques.java
+++ src/main/java/org/example/Ques.java
@@ -9,19 +9,29 @@ public class Ques {
         Scanner scanner = new Scanner(System.in);
         QuesCreate quesCreate = new QuesCreate();
         QuesPlay quesPlay = new QuesPlay();
-        System.out.println("Выберите действие:");
-        System.out.println("1. Создать викторину");
-        System.out.println("2. Начать викторину");
-        int choice = Integer.parseInt(scanner.nextLine());
-        switch (choice) {
-            case 1:
-                List<Question> questions = quesCreate.createQuestions();
-                System.out.println("Вы сделали викторину");
-                break;
-            case 2:
+        List<Question> questions = null;
+        while (true) {
+            System.out.println("Выберите действие:");
+            System.out.println("1. Создать викторину");
+            System.out.println("2. Начать викторину");
+            int choice = Integer.parseInt(scanner.nextLine());
 
+            switch (choice) {
+                case 1:
 
+                    List<Question> existingQuestions = quesCreate.createQuestions();
+                    quesPlay.playQuiz(existingQuestions);
+                    break;
+                case 2:
+                    if (questions == null) {
+                        System.out.println("Сначало создайте викторину");
+                    } else {
+                        quesPlay.playQuiz(questions);
+                    }
 
+                    break;
+
+            }
         }
     }
 }
